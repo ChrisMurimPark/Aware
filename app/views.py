3bbf61ecb661e7d55f8d45ba995f1145f0e14d43
Chris (Murim) Park <chrispark@wustl.edu>
2016-10-02 12:00:43 -0500

    Basic login form. View only.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from app import app
+from flask import render_template, redirect, flash
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html', title='Home')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Email: {}, Password: {}'.format(form.email.data,
+            form.password.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
 
