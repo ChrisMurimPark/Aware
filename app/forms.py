035d55692da99602c6e54885c487b0859d6ff110
Chris (Murim) Park <chrispark@wustl.edu>
2016-11-06 13:34:00 -0600

    Added email validator on registration

--- app/forms.py
+++ app/forms.py
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     first_name = StringField('first_name', validators=[DataRequired(),Length(1,64)])
     last_name = StringField('last_name', validators=[DataRequired(),Length(1,64)])
-    email = StringField('email', validators=[DataRequired(),Length(7,120)])
+    email = StringField('email', validators=[DataRequired(),Email(),Length(7,120)])
     password = PasswordField('password', validators=[DataRequired(),Length(4,64)])
     password_confirm = PasswordField('password_confirm',
             validators=[DataRequired(),EqualTo('password', message='Passwords do not match')])
