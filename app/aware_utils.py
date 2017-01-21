3e6acfa37403e266ac90ac8fdcca9d07bfb59e3b
Chris (Murim) Park <chrispark@wustl.edu>
2017-01-20 22:27:02 -0600

    Cleaned up db commit code and form validation code.

--- app/aware_utils.py
+++ app/aware_utils.py
@@ -9,3 +9,17 @@ def last_day_current_month():
     today = datetime.today()
     last_day = monthrange(today.year, today.month)[1]
     return date(year=today.year, month=today.month, day=last_day)
+
+# commits a sqlalchemy db session
+# returns true if success, false if failure
+def commit_db(db_session):
+    from app import db
+    from sqlalchemy.exc import IntegrityError
+    try:
+        db_session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return False
+    return True
+
+
--- app/views.py
+++ app/views.py
@@ -1,18 +1,15 @@
 from app import app, lm, db
 from flask import render_template, redirect, flash, g, request, url_for, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
-from sqlalchemy.exc import IntegrityError
 
 from .forms import LoginForm, RegisterForm, AddTransactionSingleForm, AddTransactionOverTimeForm, AddCategoryForm, StartEndDateForm
 from .models import User, Transaction, Category
 from .analytics import get_spending_by_category, get_total_spending
-from .aware_utils import first_day_current_month, last_day_current_month
+from .aware_utils import first_day_current_month, last_day_current_month, commit_db
 from .nocache import nocache
 
 import bcrypt
 import re
-from datetime import datetime, timedelta, date
-from dateutil.relativedelta import relativedelta
 
 
 @app.route('/')
@@ -20,7 +17,7 @@ from dateutil.relativedelta import relativedelta
 @login_required
 @nocache
 def index():
-    flash("Hello, {}.".format(g.user.first_name))
+    flash('Hello, {}.'.format(g.user.first_name))
     return render_template('index.html', title='Home')
 
 
@@ -29,17 +26,17 @@ def login():
     if user_set():
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter(User.email == form.email.data).first()
-        if user is None:
-            flash("Sorry, there is no user with that email.")
-            return redirect(url_for('login'))
-        if not user.verify_password(form.password.data): 
-            flash("Sorry, the password you entered is not correct.")
-            return redirect(url_for('login'))
-        login_user(user)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Login', form=form)
+    if not form.validate_on_submit():
+        return render_template('login.html', title='Login', form=form)
+    user = User.query.filter(User.email == form.email.data).first()
+    if user is None:
+        flash('Sorry, there is no user with that email.')
+        return redirect(url_for('login'))
+    if not user.verify_password(form.password.data): 
+        flash('Sorry, the password you entered is not correct.')
+        return redirect(url_for('login'))
+    login_user(user)
+    return redirect(url_for('index'))
 
 
 @app.route('/logout')
@@ -54,21 +51,18 @@ def register():
     if user_set():
         return redirect(url_for('index'))
     form = RegisterForm()
-    if form.validate_on_submit():
-        user = User(first_name=form.first_name.data,
-                last_name=form.last_name.data, 
-                email=form.email.data,
-                password=form.password.data)
-        db.session.add(user)
-        try:
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('index'))
-        except IntegrityError:
-            db.session.rollback()
-            flash('That email is already registered.')
-            return redirect(url_for('register'))
-    return render_template('register.html', title='Register', form=form)
+    if not form.validate_on_submit():
+        return render_template('register.html', title='Register', form=form)
+    user = User(first_name=form.first_name.data,
+            last_name=form.last_name.data, 
+            email=form.email.data,
+            password=form.password.data)
+    db.session.add(user)
+    if not commit_db(db.session):
+        flash('Sorry, something went wrong during registration.')
+        return redirect(url_for('register'))
+    login_user(user)
+    return redirect(url_for('index'))
 
 
 @app.route('/profile')
@@ -102,29 +96,27 @@ def add_transaction_over_time():
     form = AddTransactionOverTimeForm()
     form.category.choices = [(c.id, c.name) for c in g.user.categories.order_by(Category.name).all()]
     form.frequency.choices = [(1, 'Daily'), (2, 'Weekly'), (3, 'Monthly')]
-    if form.validate_on_submit():
-        category = Category.query.get(form.category.data)
-        name = form.title.data
-        date = form.date.data
-        cost = form.cost.data
-        freq = form.frequency.data
-        occurrences = form.occurrences.data
-        time = timedelta(1)
-        if freq is 2:
-            time = timedelta(weeks=1)
-        elif freq is 3:
-            time = relativedelta(months=+1)
-        for i in range(occurrences):
-            t = Transaction(name=name, date=date, category=category, cost=cost/occurrences, user=g.user)
-            db.session.add(t)
-            date += time
-        try:
-            db.session.commit()
-        except IntegrityError:
-            flash("Something went wrong while creating the transaction.")
-            return redirect(url_for('index'))
-        return redirect(url_for('add_transaction_result'))
-    return render_template('add_transaction_over_time.html', title='Transactions', form=form)
+    if not form.validate_on_submit():
+        return render_template('add_transaction_over_time.html', title='Transactions', form=form)
+    category = Category.query.get(form.category.data)
+    name = form.title.data
+    date = form.date.data
+    cost = form.cost.data
+    freq = form.frequency.data
+    occurrences = form.occurrences.data
+    time = timedelta(1)
+    if freq is 2:
+        time = timedelta(weeks=1)
+    elif freq is 3:
+        time = relativedelta(months=+1)
+    for i in range(occurrences):
+        t = Transaction(name=name, date=date, category=category, cost=cost/occurrences, user=g.user)
+        db.session.add(t)
+        date += time
+    if not commit_db(db.session):
+        flash('Sorry, something went wrong while creating the transaction.')
+        return redirect(url_for('index'))
+    return redirect(url_for('add_transaction_result'))
 
 
 @app.route('/add_transaction/single', methods=['GET', 'POST'])
@@ -133,20 +125,18 @@ def add_transaction_over_time():
 def add_transaction_single():
     form = AddTransactionSingleForm()
     form.category.choices = [(c.id, c.name) for c in g.user.categories.order_by(Category.name).all()]
-    if form.validate_on_submit():
-        category = Category.query.get(form.category.data)
-        name = form.title.data
-        date = form.date.data
-        cost = form.cost.data
-        t = Transaction(name=name, date=date, category=category, cost=cost, user=g.user)
-        db.session.add(t)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            flash("Something went wrong while creating the transaction.")
-            return redirect(url_for('index'))
-        return redirect(url_for('add_transaction_result'))
-    return render_template('add_transaction_single.html', title='Transactions', form=form)
+    if not form.validate_on_submit():
+        return render_template('add_transaction_single.html', title='Transactions', form=form)
+    category = Category.query.get(form.category.data)
+    name = form.title.data
+    date = form.date.data
+    cost = form.cost.data
+    t = Transaction(name=name, date=date, category=category, cost=cost, user=g.user)
+    db.session.add(t)
+    if not commit_db(db.session):
+        flash('Sorry, something went wrong while creating the transaction.')
+        return redirect(url_for('index'))
+    return redirect(url_for('add_transaction_result'))
 
 
 @app.route('/add_transaction/result')
@@ -166,13 +156,11 @@ def delete_transaction():
         if d is not -1:
             transaction = Transaction.query.filter(Transaction.id == d).first() 
             db.session.delete(transaction)
-    try:
-        db.session.commit()
-    except IntegrityError:
-        flash("Sorry! Something went wrong while deleting the transaction(s).")
-        return jsonify(result="Sorry!")
-    flash("Successfully deleted transaction(s).")
-    return jsonify("Success!")
+    if not commit_db(db.session):
+        flash('Sorry, Something went wrong while deleting the transaction(s).')
+        return jsonify(result='Sorry!')
+    flash('Successfully deleted transaction(s).')
+    return jsonify('Success!')
 
 
 @app.route('/categories')
@@ -188,15 +176,13 @@ def categories():
 @nocache
 def add_category():
     form = AddCategoryForm()
-    if form.validate_on_submit():
-        db.session.add(Category(name=form.name.data, user=g.user))
-        try:
-            db.session.commit()
-        except IntegrityError:
-            flash("Something went wrong while creating the transaction.")
-            return redirect(url_for('index'))
-        return redirect(url_for('categories'))
-    return render_template('add_category.html', title='Categories', form=form)
+    if not form.validate_on_submit():
+        return render_template('add_category.html', title='Categories', form=form)
+    db.session.add(Category(name=form.name.data, user=g.user))
+    if not commit_db(db.session):
+        flash('Sorry, something went wrong while creating the transaction.')
+        return redirect(url_for('index'))
+    return redirect(url_for('categories'))
 
 
 @app.route('/analytics', methods=['GET','POST'])
